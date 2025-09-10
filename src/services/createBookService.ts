7f2c33d084c31c2bef486c333a5557c4ba2910aa
LSL Web Development <lslwebdevelopment@gmail.com>
2025-09-10 11:08:55 -0300

    Updated the url

--- src/services/createBookService.ts
+++ src/services/createBookService.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
 import type { IBookCreate, IBookEntity } from "../entities/books";
 
-const url = "https://book-list-back-end.onrender.com/books";
-// const url = import.meta.env.VITE_API_URL;
+// const url = "https://book-list-back-end.onrender.com/books";
+const url = import.meta.env.VITE_API_URL;
 
 export async function createBookService(
   data: IBookCreate
 ): Promise<IBookEntity> {
   const response = await axios.post<IBookEntity>(url, data);
   // console.log("createBookService: ", response.data);
+  console.log("API URL:", import.meta.env.VITE_API_URL);
   return response.data;
 }
