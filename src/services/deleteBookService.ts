7a002ba9d07147d1cba278c2f5b9b41aa440a35d
LSL Web Development <lslwebdevelopment@gmail.com>
2025-09-10 11:54:02 -0300

    Added functionality to delete books

--- src/services/deleteBookService.ts
+++ src/services/deleteBookService.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import type { IBookEntity } from "../entities/books";
 
-const url = "https://book-list-back-end.onrender.com/books";
-// const url = import.meta.env.VITE_API_URL;
+// const url = "https://book-list-back-end.onrender.com/books";
+const url = import.meta.env.VITE_API_URL;
 
 export async function deleteBookService(id: string): Promise<IBookEntity> {
   const response = await axios.delete<IBookEntity>(`${url}/${id}`);
